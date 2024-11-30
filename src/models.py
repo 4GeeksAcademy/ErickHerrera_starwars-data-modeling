f9e6d71a8c6b7dd1f1e82f133d6b4893893ee5b1
10ErickHerrera <10erickherrera@gmail.com>
2024-11-30 02:07:29 +0000

    Finished

--- src/models.py
+++ src/models.py
@@ -1,29 +1,51 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuarios'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    favoritos = relationship('Favorito', back_populates='usuario')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planeta(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(50), nullable=False)
+    descripcion = Column(String)
+    clima = Column(String(50), nullable=False)  
+    terreno = Column(String(100), nullable=False)  
+    gravedad = Column(String(50), nullable=False)  
+    populacion = Column(Integer, nullable=True)  
+    favoritos = relationship('Favorito', back_populates='planeta')
+
+class Personaje(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    descripcion = Column(String)
+    edad = Column(Integer, nullable=True)  
+    peso = Column(Float, nullable=True)  
+    raza = Column(String(50), nullable=True)  
+    color_ojos = Column(String(50), nullable=True)  
+    favoritos = relationship('Favorito', back_populates='personaje')
+
+class Favorito(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    id_planeta = Column(Integer, ForeignKey('planetas.id'), nullable=True)
+    id_personaje = Column(Integer, ForeignKey('personajes.id'), nullable=True)
+    
+    usuario = relationship(Usuario, back_populates='favoritos')
+    planeta = relationship(Planeta, back_populates='favoritos')
+    personaje = relationship(Personaje, back_populates='favoritos')
 
     def to_dict(self):
         return {}
